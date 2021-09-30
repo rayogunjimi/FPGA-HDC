b1a677c278ff9718fdc94bd49336a9cc049f98a1
Matt Bicknese <mbicknes@villanova.edu>
2021-09-30 16:11:42 -0400

    Refactored Code for Ham and Spam Vectors

--- Hamming.v
+++ Hamming.v
@@ -1,18 +1,29 @@
-module Hamming(a,b, dist);
-    input [9999:0] a, b;
-    output dist;
+module Hamming(a, hamVector, spamVector, dist);
+    input [9999:0] a, hamVector, spamVector;
+    output HamSpam;
 
     integer i;
-    integer count;
+    integer countHam;
+    integer countSpam;
 
-    count <= 1;
+    countHam <= 0;
+    countSpam <= 0;
 
     initial begin
         for (i=0; i<10000; i = i+1) begin
-            if (a[i] ^ b[i] == 1)
-                count = count + 1;
+            if (a[i] ^ hamVector[i] == 1)
+                countHam = countHam + 1;
+            else if (a[i] ^ spamVector[i] == 1) begin
+                countSpam = countSpam + 1;
         end
     end
-    dist <= count;
+
+    if (countHam > countSpam)
+        HamSpam <= -1;
+    else if (countHam < countSpam) begin
+        HamSpam <= 1;
+    end else
+        HamSpam <= 0;
+    
 endmodule
 
