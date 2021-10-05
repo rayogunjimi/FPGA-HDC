bfb5ecf350ede5ea27df77a0043147153d2aedaa
Ray Ogunjimi <81774312+rayogunjimi@users.noreply.github.com>
2021-10-05 17:57:52 -0400

    add length and label input as well as output parameter

--- main_tb.v
+++ main_tb.v
@@ -1,3 +1,4 @@
+`import main.v
 `timescale 1 ns/10 ps  // time-unit = 1 ns, precision = 10 ps
 
 module fpga_hdc_tb()
@@ -17,14 +18,23 @@ string test_path_list [0:numof_tests-1];
 
 // file read handling
 integer data_file;
+integer length_file;
+integer label_file;
+
 integer scan_file;
 
+// inputs to main function
 // vector of strings (max 160 characters per text) (max 32 bits/character)
-reg [31:0] input_vector [160:0];
+	reg [31:0] input_data [160:0];
+reg input_label;
+reg input_length;
+
+// output of main function
+wire output_classification;
 
 localparam period = 20;  
 
-tokenizer_module DUT(.a(input_vector));
+	tokenizer_module DUT(.a(input_data));
 
 initial begin
 	// file paths
@@ -41,6 +51,8 @@ initial begin
 	
 	for(i = 0; i < numof_tests; i = i + 1) begin
 		data_file = $fopen(test_path_list [i], "r");
+		label_file = $fopen(dictionary_path_list [i], "r");
+		length_file = $fopen(length_path_list [i], "r");
 		if (data_file == `NULL) begin
 			$display("data_file handle was NULL");
 		end // if
@@ -49,24 +61,35 @@ initial begin
 		end // else
 	end // for
 
-	scan_file = $fscanf(data_file, "%d\n", input_vector); 
+	scan_file = $fscanf(data_file, "%d\n", input_data); 
+	scan_file = $fscanf(label_file, "%d\n", input_label); 
+	scan_file = $fscanf(length_file, "%d\n", input_length); 
 	if (!$feof(data_file)) begin
 		
-	end
+	end // if
 
 end // initial
 
 always @(posedge clk) begin
 
 	#period clk = ~clk;
-end
+end // always
+
+// add logic to check return value (ham/spam) with disctonary
+// add logic to pass in diffierent hypervector instead of hard coded
+
+// call main module
+main U_main (
+input_data,
+input_label,
+input_length,
+output_classification
+);
+
+end // module
 
 // forever begin
 // end // forever
-
-// add logic to check return value (ham/spam) with disctonary
- 
-end // module
 
 // exit simulation
 // $finish
